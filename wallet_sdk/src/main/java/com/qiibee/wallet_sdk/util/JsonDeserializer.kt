b7b50968c2ff033c9b77caeb8fcc777f2ca365e9
leviathanbeak88 <elvisdedic@outlook.com>
2019-12-12 16:23:49 +0100

    improved json deserializer

--- wallet_sdk/src/main/java/com/qiibee/wallet_sdk/util/JsonDeserializer.kt
+++ wallet_sdk/src/main/java/com/qiibee/wallet_sdk/util/JsonDeserializer.kt
@@ -1,75 +1,188 @@
 package com.qiibee.wallet_sdk.util
 
+import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 import com.qiibee.wallet_sdk.client.*
+import org.web3j.crypto.Hash
+import org.web3j.crypto.RawTransaction
 import java.math.BigDecimal
+import java.sql.Timestamp
 
 internal object JsonDeserializer {
-    fun deserializeTokenBalances(content: String): TokenBalances {
-        val balancesHelper = Gson().fromJson(content, TokenBalancesDeserializerHelper::class.java)
-        val privateTokens = balancesHelper.balances.formatPrivateTokens()
-        val ethBalance = balancesHelper.balances.formatETHBalance()
-        val publicTokens = balancesHelper.balances.formatPublicTokens()
+    class TokenBalancesDeserializer: ResponseDeserializable<TokenBalances> {
+        override fun deserialize(content: String): TokenBalances {
+            val balancesHelper = Gson().fromJson(content, TokenBalancesDeserializerHelper::class.java)
+            val privateTokens = balancesHelper.balances.fromPrivateTokens()
+            val ethBalance = balancesHelper.balances.fromETHBalance()
+            val publicTokens = balancesHelper.balances.fromPublicTokens()
 
-        return TokenBalances(
-            balancesHelper.transactionCount,
-            Balances(privateTokens, publicTokens, ethBalance),
-            balancesHelper.aggregateValue
+            return TokenBalances(
+                balancesHelper.transactionCount,
+                Balances(privateTokens, publicTokens, ethBalance),
+                balancesHelper.aggregateValue
+            )
+        }
+    }
+
+    class RawTxDeserializer: ResponseDeserializable<RawTransaction> {
+        override fun deserialize(content: String): RawTransaction {
+            return Gson().fromJson(content, RawTransaction::class.java)
+        }
+    }
+
+    class TokensDeserializer: ResponseDeserializable<Tokens> {
+        override fun deserialize(content: String): Tokens {
+            val tokensHelper = Gson().fromJson(content, TokensDeserializerHelper::class.java)
+            val privateTokens = tokensHelper.fromPrivateTokens()
+            val publicTokens = tokensHelper.fromPublicTokens()
+
+            return Tokens(privateTokens, publicTokens)
+        }
+    }
+
+    class TransactionsDeserializer: ResponseDeserializable<List<Transaction>> {
+        override fun deserialize(content: String): List<Transaction> {
+            val listType = object : TypeToken<List<TransactionIntermediate>>() {}.type
+            return Gson().fromJson<List<TransactionIntermediate>>(content, listType).map {
+                it.formatTransaction()
+            }
+        }
+    }
+
+    class SendTransactionDeserializer: ResponseDeserializable<Hash> {
+        override fun deserialize(content: String): Hash {
+            return Gson().fromJson(content, Hash::class.java)
+        }
+    }
+
+}
+
+private data class TransactionIntermediate(
+    val to: String,
+    val from: String,
+    val hash: String,
+    val contractAddress: String,
+    val timestamp: Long,
+    val token: TokenIntermediate
+) {
+    fun formatTransaction(): Transaction {
+        return Transaction(
+            WalletAddress(to),
+            WalletAddress(from),
+            WalletAddress(contractAddress),
+            Timestamp(timestamp),
+            token.formatToToken()
         )
     }
 }
 
-internal data class BalancesDeserializerHelper(
-    @SerializedName("private") val privateTokens: Any,
-    @SerializedName("public") val publicTokens: Any
+private data class TokensDeserializerHelper (
+    @SerializedName("private") val privateTokens: List<TokenIntermediate>,
+    @SerializedName("public") val publicTokens: List<TokenIntermediate>
 ) {
-    fun formatPrivateTokens(): List<Token> {
-        val stringify = privateTokens.toString()
-        val clean = stringify.substring(1, stringify.length - 1)
-        val split = clean.split(",")
 
-        val tokens = mutableListOf<Token>()
-        for (i in 0 until split.size step 2) {
-            tokens.add((createToken(split[i], split[i+1])))
+    fun fromPrivateTokens(): List<Token> {
+        val list = mutableListOf<Token>()
+        for (token in privateTokens) {
+            list.add(token.formatToToken())
         }
-
-        return tokens
+        return list
     }
 
-    fun formatPublicTokens(): List<Token> {
-        val stringify = publicTokens.toString()
-        val clean = stringify.substring(1, stringify.length - 1)
-        val split = clean.split(",").filter { p -> !p.contains("ETH") }
-
-        val tokens = mutableListOf<Token>()
-        for (i in 0 until split.size step 2) {
-            tokens.add((createToken(split[i], split[i+1])))
+    fun fromPublicTokens(): List<Token> {
+        val list = mutableListOf<Token>()
+        val filteredTokens = publicTokens.filter { token -> token.symbol != Constants.ETH }
+        for (token in filteredTokens) {
+            list.add(token.formatToToken())
         }
-        return tokens
+        return list
     }
 
-    fun formatETHBalance() : ETHBalance {
-        val stringify = publicTokens.toString()
-        val clean = stringify.substring(1, stringify.length - 1)
-        val ethString = clean.split(",").find { p -> p.contains("ETH") }
-        val ethValue = ethString?.split("=")!![2]
+}
 
-        return ETHBalance(BigDecimal(ethValue.substring(0, ethValue.length - 1)))
-    }
-
-    fun createToken(nameBalance: String, contractAddressValue: String): Token {
-        val nameBalanceSplit = nameBalance.split("={")
-        val name = nameBalanceSplit[0].trim()
-        val balance = nameBalanceSplit[1].split("=")[1]
-        val address = contractAddressValue.substring(0, contractAddressValue.length-1).split("=")[1]
-
-        return Token(TokenName(name), BigDecimal(balance), WalletAddress(address))
+private data class TokenIntermediate (
+    val balance: String?,
+    val contractAddress: String,
+    val decimals: Number,
+    val description: String,
+    val logoUrl: String,
+    val name: String,
+    val symbol: String,
+    val totalSupply: String,
+    val website: String
+) {
+    fun formatToToken(): Token {
+        return Token(
+            TokenSymbol(symbol),
+            BigDecimal(balance?:"0"),
+            WalletAddress(contractAddress)
+        )
     }
 }
 
-internal data class TokenBalancesDeserializerHelper(
+private data class TokenBalancesDeserializerHelper(
     val transactionCount: Int,
     val balances: BalancesDeserializerHelper,
     val aggregateValue: AggregateValue
 )
+
+private data class BalancesDeserializerHelper(
+    @SerializedName("private") val privateTokens: Any,
+    @SerializedName("public") val publicTokens: Any
+) {
+
+    fun fromPrivateTokens(): List<Token> {
+        val map = Gson().fromJson(privateTokens.toString(), Map::class.java)
+        val tokens = mutableListOf<Token>()
+
+        for ((k,v) in map) {
+            val props = Gson().fromJson(v.toString(), BalanceContractIntermediate::class.java)
+            val token = createToken(k.toString(), props.balance, props.contractAddress)
+            tokens.add(token)
+        }
+
+        return tokens
+    }
+
+    fun fromPublicTokens(): List<Token> {
+        val map = Gson().fromJson(publicTokens.toString(), Map::class.java)
+        val tokens = mutableListOf<Token>()
+
+        for ((k, v) in map) {
+            if (k.toString() != Constants.ETH) {
+                val props = Gson().fromJson(v.toString(), BalanceContractIntermediate::class.java)
+                val token = createToken(k.toString(), props.balance, props.contractAddress)
+                tokens.add(token)
+            }
+        }
+        return tokens
+    }
+
+    fun fromETHBalance(): ETHBalance {
+        val map = Gson().fromJson(publicTokens.toString(), Map::class.java)
+
+        for ((key, value) in map) {
+            if (key.toString() == Constants.ETH) {
+                val props = Gson().fromJson(value.toString(), BalanceIntermediate::class.java)
+                return ETHBalance(BigDecimal(props.balance))
+            }
+        }
+
+        return ETHBalance(BigDecimal(0))
+    }
+
+    private fun createToken(symbol: String, balance: String, address: String): Token {
+        return Token(TokenSymbol(symbol), BigDecimal(balance), WalletAddress(address))
+    }
+
+    private data class BalanceContractIntermediate(
+        val balance: String,
+        val contractAddress: String
+    )
+
+    private data class BalanceIntermediate(
+        val balance: String
+    )
+}
