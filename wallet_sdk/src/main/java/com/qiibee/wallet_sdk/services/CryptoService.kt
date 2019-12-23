a1f3458a90ea257e81eaba59e52cc0d295cd9007
leviathanbeak88 <elvisdedic@outlook.com>
2019-12-23 07:55:33 +0100

    added more details on implementation

--- wallet_sdk/src/main/java/com/qiibee/wallet_sdk/services/CryptoService.kt
+++ wallet_sdk/src/main/java/com/qiibee/wallet_sdk/services/CryptoService.kt
@@ -13,7 +13,8 @@ import java.security.SecureRandom
 internal object CryptoService: CryptoProvider {
     override fun createMnemonic(): Result<Mnemonic, Exception> {
         return try {
-            val initialEntropy = ByteArray(16)
+            val arraySize = 16
+            val initialEntropy = ByteArray(arraySize)
             val secureRandom = SecureRandom()
             secureRandom.nextBytes(initialEntropy)
             val mnemonic = MnemonicUtils.generateMnemonic(initialEntropy)
@@ -36,10 +37,12 @@ internal object CryptoService: CryptoProvider {
         return Credentials.create(privateKey.privateKey)
     }
 
+    // Custom function for creating Credentials in order to avoid
+    // problem with different privateKey being generated compered to other Crypto libraries
     private fun createCredentialsFrom(mnemonic: Mnemonic): Credentials {
         // "m/44'/60'/0'/0/0"
         val path = intArrayOf(44 or HARDENED_BIT, 60 or HARDENED_BIT, 0 or HARDENED_BIT, 0, 0)
-        val seed = MnemonicUtils.generateSeed(mnemonic.phrase,"")
+        val seed = MnemonicUtils.generateSeed(mnemonic.phrase, "")
         val masterKeyPair = Bip32ECKeyPair.generateKeyPair(seed)
         val bip44Keypair = Bip32ECKeyPair.deriveKeyPair(masterKeyPair, path)
 
