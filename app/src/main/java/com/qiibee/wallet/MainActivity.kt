978e711ee7b53a0933eaee08f36d97c5371e6583
leviathanbeak88 <elvisdedic@outlook.com>
2020-01-02 11:41:11 +0100

    added example code in app

--- app/src/main/java/com/qiibee/wallet/MainActivity.kt
+++ app/src/main/java/com/qiibee/wallet/MainActivity.kt
@@ -6,28 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.qiibee.wallet_sdk.client.CryptoWallet
+import com.qiibee.wallet_sdk.client.Mnemonic
 import com.qiibee.wallet_sdk.client.WalletAddress
 import com.qiibee.wallet_sdk.util.Failure
-import com.qiibee.wallet_sdk.util.Logger
 import com.qiibee.wallet_sdk.util.Success
-
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
+
+    private val INFO = "INFO_LOG"
+    private val ERROR = "ERROR_LOG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        when (CryptoWallet.loadWallet()) {
-            is Success -> {
-                getTokens()
-                loadBalances()
-                getTransactions()
-            }
-            is Failure -> Logger.log("LOAD WALLET FAILED")
-        }
+        // Initialize Context
+        CryptoWallet.initialize(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -46,46 +43,133 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun loadBalances() {
-        CryptoWallet.getBalances { result ->
-            when (result) {
+    fun getWalletAddress() {
+        when (val result = CryptoWallet.walletAddress()) {
+            is Success -> {
+                Log.d(INFO, "wallet address is ${result.value.address}")
+            }
+            is Failure -> {
+                Log.e(ERROR, "failed with ${result.reason.message}")
+            }
+        }
+    }
+
+    fun getPrivateKey() {
+        when (val result = CryptoWallet.privateKey()) {
+            is Success -> {
+                Log.d(INFO, "private key is ${result.value.privateKey}")
+            }
+            is Failure -> {
+                Log.e(ERROR, "failed with ${result.reason.message}")
+            }
+        }
+    }
+
+    fun getMnemonicPhrase() {
+        when (val result = CryptoWallet.mnemonicPhrase()) {
+            is Success -> {
+                Log.d(INFO, "phrase is ${result.value.phrase}")
+            }
+            is Failure -> {
+                Log.e(ERROR, "failed with ${result.reason.message}")
+            }
+        }
+    }
+
+    fun createWallet() {
+        when (val result = CryptoWallet.createWallet()) {
+            is Success -> {
+                Log.d(INFO, "created wallet with address: ${result.value.address}")
+            }
+            is Failure -> {
+                Log.e(ERROR, "failed with ${result.reason.message}")
+            }
+        }
+    }
+
+    fun restoreWallet() {
+        val validMnemonic = Mnemonic("station hard under bus always prompts film story tires valid fake phrase")
+        when (val result = CryptoWallet.restoreWallet(validMnemonic)) {
+            is Success -> {
+                Log.d(INFO, "restored wallet with address: ${result.value.address}")
+            }
+            is Failure -> {
+                Log.e(ERROR, "failed with ${result.reason.message}")
+            }
+        }
+    }
+
+    fun removeStoredWallet() {
+        when (val result = CryptoWallet.removeWallet()) {
+            is Success -> {
+                Log.d(INFO, "wallet successfully removed")
+            }
+            is Failure -> {
+                Log.e(ERROR, "failed with ${result.reason.message}")
+            }
+        }
+    }
+
+    fun getBalances() {
+        CryptoWallet.getBalances {
+            when (it) {
                 is Success -> {
-                    Logger.log(result.value.balances.ethBalance.balance.toString())
+                    Log.d(INFO, "balances: ${it.value.balances}")
                 }
                 is Failure -> {
-                    Logger.log(result.toString())
+                    Log.e(ERROR, "failed with ${it.reason.message}")
                 }
             }
         }
     }
 
     fun getTokens() {
-        CryptoWallet.getTokens { result ->
-            when (result) {
+        CryptoWallet.getTokens {
+            when (it) {
                 is Success -> {
-                    for (token in result.value.privateTokens) {
-                        Logger.log(token.contractAddress.address)
-                    }
+                    Log.d(INFO, "public tokens: ${it.value.publicTokens}, private tokens ${it.value.privateTokens}")
                 }
                 is Failure -> {
-                    Logger.log(result.toString())
+                    Log.e(ERROR, "failed with ${it.reason.message}")
                 }
             }
         }
     }
 
     fun getTransactions() {
-        CryptoWallet.getTransactions { result ->
-            when (result) {
+        CryptoWallet.getTransactions{
+            when (it) {
                 is Success -> {
-                    for (tx in result.value) {
-                        Logger.log(tx.toString())
+                    for (tx in it.value) {
+                        Log.d(INFO, "contractAddress: ${tx.contractAddress} - from:${tx.from}")
                     }
                 }
                 is Failure -> {
-                    Logger.log(result.toString())
+                    Log.e(ERROR, "failed with ${it.reason.message}")
                 }
             }
         }
     }
+
+    fun sendTransaction() {
+        val toAddress = WalletAddress("valid address here")
+        val contractAddress = WalletAddress("valid address here")
+        val value = BigDecimal(0.324)
+
+        CryptoWallet.sendTransaction(toAddress, contractAddress, value){
+            when (it) {
+                is Success -> {
+                    Log.d(INFO, "Hash of the transactions is ${it.value}")
+                }
+                is Failure -> {
+                    Log.e(ERROR, "failed with ${it.reason.message}")
+                }
+            }
+        }
+    }
+
+
+
+
+
 }
