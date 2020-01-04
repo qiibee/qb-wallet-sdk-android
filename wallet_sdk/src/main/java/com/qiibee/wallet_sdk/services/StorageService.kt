4a0af3b8595f8310504257642cfd0cf6f1d81ed0
leviathanbeak88 <elvisdedic@outlook.com>
2020-01-04 08:31:27 +0100

    added ZERO_PREFIX to private key before storing it

--- wallet_sdk/src/main/java/com/qiibee/wallet_sdk/services/StorageService.kt
+++ wallet_sdk/src/main/java/com/qiibee/wallet_sdk/services/StorageService.kt
@@ -60,8 +60,7 @@ internal object StorageService: StorageProvider {
         mnemonic: Mnemonic
     ): Result<Unit, Exception> {
         return try {
-            val privateKey = Numeric.toHexStringWithPrefix(credentials.ecKeyPair.privateKey)
-
+            val privateKey = formatPrivateKey(credentials)
             SecurePreferences.setValue(context, Constants.PRIVATE_KEY, privateKey)
             SecurePreferences.setValue(context, Constants.WALLET_ADDRESS, credentials.address)
             SecurePreferences.setValue(context, Constants.MNEMONIC_PHRASE, mnemonic.phrase)
@@ -80,4 +79,8 @@ internal object StorageService: StorageProvider {
         }
     }
 
+    private fun formatPrivateKey(credentials: Credentials): String {
+        return Constants.ZERO_PREFIX +
+                Numeric.toHexStringNoPrefix(credentials.ecKeyPair.privateKey)
+    }
 }
