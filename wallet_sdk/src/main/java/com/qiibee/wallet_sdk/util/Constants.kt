e0a40b55a878606fb2d5fb4c4909bca40cf88be4
leviathanbeak88 <elvisdedic@outlook.com>
2020-01-04 08:31:37 +0100

    added ZERO_PREFIX constant

--- wallet_sdk/src/main/java/com/qiibee/wallet_sdk/util/Constants.kt
+++ wallet_sdk/src/main/java/com/qiibee/wallet_sdk/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val MNEMONIC_PHRASE = "MNEMONIC_PHRASE"
     const val PRIVATE_KEY = "PRIVATE_KEY"
     const val WALLET_ADDRESS = "WALLET_ADDRESS"
+    const val ZERO_PREFIX = "0x0"
 }
